e652e05bae0864925d3cfdfc8823fbfb892bfeff
Emmanuel <emmanuel77fg@yahoo.fr>
2023-11-15 18:02:46 -0500

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        GestionnaireDeCompte gestionnaire = new GestionnaireDeCompte();
+        String pseudo = getUserInput("Please enter your pseudo:");
+        String nom = getUserInput("Please enter your name:");
+        String prenom = getUserInput("Please enter your surname:");
+        String addresseCourriel = getUserInput("Please enter your email address:");
+        String motDePasse = getUserInput("Please enter your password:");
+        String telephoneStr = getUserInput("Please enter your phone number:");
+        Acheteur nouvelAcheteur = Acheteur.inscrire(gestionnaire, pseudo, nom, prenom, addresseCourriel, motDePasse, telephoneStr);
     }
-}
+
+
+    public static String getUserInput(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+    }
---
+++ src/Utilisateur.java
@@ -0,0 +1,31 @@
+    public class Utilisateur {
+        private String nom, prenom, addresseCourriel, motDePasse,telephone;
+
+        public Utilisateur (String nom, String prenom, String addresseCourriel, String motDePasse, String telephone){
+            this.nom = nom;
+            this.prenom = prenom;
+            this.addresseCourriel = addresseCourriel;
+            this.motDePasse= motDePasse;
+            this.telephone = telephone;
+        }
+
+        public void setAddresseCourriel(String addresseCourriel) {
+            this.addresseCourriel = addresseCourriel;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+        public void setPrenom(String prenom) {
+            this.prenom = prenom;
+        }
+
+        public void setTelephone(String telephone) {
+            this.telephone = telephone;
+        }
+        public void setMotDePasse(String motDePasse) {
+            this.motDePasse = motDePasse;
+        }
+
+
+    }
