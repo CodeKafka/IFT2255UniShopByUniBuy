c0dec82d1bfdb0440b18a54d6a9f2330e5ec2a4c
Emmanuel <emmanuel77fg@yahoo.fr>
2023-11-15 18:43:23 -0500

    first

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,39 @@ public class Main {
     public static void main(String[] args) {
 
         GestionnaireDeCompte gestionnaire = new GestionnaireDeCompte();
-        String pseudo = getUserInput("Please enter your pseudo:");
-        String nom = getUserInput("Please enter your name:");
-        String prenom = getUserInput("Please enter your surname:");
-        String addresseCourriel = getUserInput("Please enter your email address:");
-        String motDePasse = getUserInput("Please enter your password:");
-        String telephoneStr = getUserInput("Please enter your phone number:");
-        Acheteur nouvelAcheteur = Acheteur.inscrire(gestionnaire, pseudo, nom, prenom, addresseCourriel, motDePasse, telephoneStr);
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+        String pseudo = getUserInput("Please enter your pseudo (letters and numbers only):", "^[a-zA-Z0-9]+$");
+        String nom = getUserInput("Please enter your name (letters and hyphens allowed):", "^[a-zA-Z-]+$");
+        String prenom = getUserInput("Please enter your surname (letters and hyphens allowed):", "^[a-zA-Z-]+$");
+        String addresseCourriel = getUserInput("Please enter your email address:",emailRegex);
+        String motDePasse = getUserInput("Please enter your password (letters and numbers only):", "^[a-zA-Z0-9]+$");
+        String telephone = getUserInput("Please enter your phone number (numbers only):", "^[0-9]+$");
+        Acheteur nouvelAcheteur = Acheteur.inscrire(
+                gestionnaire,
+                pseudo,
+                nom,
+                prenom,
+                telephone,
+                motDePasse,
+                addresseCourriel
+        );
     }
 
 
-    public static String getUserInput(String prompt) {
-        System.out.println(prompt);
-        return scanner.nextLine();
+    public static String getUserInput(String prompt, String regex) {
+        String input;
+        while (true) {
+            System.out.println(prompt);
+            input = scanner.nextLine();
+            if (input.matches(regex)) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+        return input;
     }
-    }
+
+}
+
