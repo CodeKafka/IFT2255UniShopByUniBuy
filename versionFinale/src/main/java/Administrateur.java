ba86c74546b2ec4390dd3e79e60ed37276f690cf
Emmanuel <99663916+MufasaKhan@users.noreply.github.com>
2023-11-26 10:23:01 -0500

    Add files via upload

---
+++ versionFinale/src/main/java/Administrateur.java
@@ -0,0 +1,17 @@
+public class Administrateur  {
+    private String identifiantAdmin, modDePasseAdmin; 
+    private  int idAdmin;
+
+    public boolean supprimerCompte(Utilisateur utilisateur) {
+        /* À copléter  */
+        return false;
+    }
+
+    public void creerCompteAcheteur(String pseudo, String nom, String prenom, String adresseCourriel, String telephone, String adresseExpedition) {
+        /* À compléter */
+    }
+    
+    public void creerCompteRevendeur(String nomEntreprise, String adresseEntreprise, String adresseCourriel, String telephone, Produit produit) {
+        /* À compléter */
+    } 
+}
---
+++ versionFinale/src/main/java/GestionnaireDeRecherche.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class GestionnaireDeRecherche {
+    
+  public static ArrayList<Produit> afficherCatalogueProduits() {
+    return new ArrayList<Produit>();
+    }  
+}
