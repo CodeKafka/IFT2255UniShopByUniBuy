75c765692256bba7b96e1a96dacae32af3aa2d8f
Emmanuel <emmanuel77fg@yahoo.fr>
2023-11-26 10:56:56 -0500

    Ajout de maven

---
+++ Maven/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Panier panier = new Panier();
+        Produit cal = new Produit(567,"Caculator","Hel","Yup",1,20.5);
+
+        Acheteur acheteur = new Acheteur("Bob","Marley","Hello","123","123","yea");
+
+        System.out.println(acheteur.getPanier());
+
+        Vue vue = new Vue();
+        Controleur controleur = new Controleur(vue);
+        if (Controleur.verifierExistanceFichierCSVUtilisateurs()) {
+            Controleur.initialiserBaseDeDonneesUtilisateurs();
+        }
+       controleur.demarrerApplication();
+    }
+
+}
+
